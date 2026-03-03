f87b5e5fb7d0a1fb873fc308251cbe118c7edfe1
jathin317 <jathin317@gmail.com>
2026-03-03 23:58:40 +0530

    Initial grid setup and turn

---
+++ ChainReaction.py
@@ -0,0 +1,30 @@
+import os
+
+rows = 5
+cols = 5
+
+orbs_grid = [[0 for _ in range(cols)] for _ in range(rows)]
+
+occupied = [['n' for _ in range(cols)] for _ in range(rows)]
+
+def turn(player):
+    row = int(input("row: "))
+    col = int(input("col: "))
+
+    orbs_grid[row][col] += 1
+    occupied[row][col] = player
+
+    os.system('clear')
+    print_grid()
+    turn('p2')
+
+
+
+def print_grid():
+    for i in range(rows):
+        for j in range(cols):
+            print(f"{occupied[i][j]}:{orbs_grid[i][j]} ", end = "")
+        print()
+
+print_grid()
+turn('p1')
