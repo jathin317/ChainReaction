1a2878d36c38fe55b02ab7ad628c70b34e9d9fff
jathin317 <jathin317@gmail.com>
2026-03-04 00:08:34 +0530

    formatted output and fixed a minor bug in turn function

--- ChainReaction.py
+++ ChainReaction.py
@@ -5,25 +5,32 @@ cols = 5
 
 orbs_grid = [[0 for _ in range(cols)] for _ in range(rows)]
 
-occupied = [['n' for _ in range(cols)] for _ in range(rows)]
+occupied = [['n ' for _ in range(cols)] for _ in range(rows)]
 
 def turn(player):
     row = int(input("row: "))
     col = int(input("col: "))
+
+    if not(0 <= row < rows and 0 <= col < cols):
+        print("Invalid input")
+        turn(player)
 
     orbs_grid[row][col] += 1
     occupied[row][col] = player
 
     os.system('clear')
     print_grid()
-    turn('p2')
+    if player == 'p2':
+        turn('p1')
+    else:
+        turn('p2')
 
 
 
 def print_grid():
     for i in range(rows):
         for j in range(cols):
-            print(f"{occupied[i][j]}:{orbs_grid[i][j]} ", end = "")
+            print(f" {occupied[i][j]}:{orbs_grid[i][j]} |", end = "")
         print()
 
 print_grid()
